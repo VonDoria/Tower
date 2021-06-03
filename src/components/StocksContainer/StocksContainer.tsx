9fc7b1be894cb1694d543e352ad90f8f8da27439
ITALO DORIA <italo.faria0@gmail.com>
2021-06-03 17:17:56 -0300

    Card

--- src/components/StocksContainer/StocksContainer.tsx
+++ src/components/StocksContainer/StocksContainer.tsx
@@ -1,7 +1,33 @@
 import { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 import styles from './StocksContainer.module.css';
 
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    margin: 5,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+
 export default function Home() {
+
+  const classes = useStyles();
 
   const [selectedStocks, setSelectedStocks] = useState([]);
 
@@ -14,13 +40,23 @@ export default function Home() {
   return (
     <div className={styles.container}>
       {selectedStocks.map((element, index) => {
-        return(
-            <div key={index} className={styles.card}>
-                <div>{element.name}</div>
-                <div>{element.code}</div>
-                <div>{element.variation}</div>
-                <div>{element.price}</div>
-            </div>
+        return(            
+            <Card key={index} className={classes.root} variant="outlined">
+              <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                  {element.code}
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {element.name}
+                </Typography>
+                <Typography className={classes.pos} color="textSecondary">
+                  {element.price}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {element.variation}
+                </Typography>
+              </CardContent>
+            </Card>
         );
       })}
     </div>
